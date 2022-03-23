f291fd663cd79f303bfa8a70a827c604222ca2fd
artpasut <pasut67@gmail.com>
2022-03-23 12:01:34 +0700

    [ecr.tf] fix naming resource

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "this" {
-  name                 = var.repository_name
+  name                 = "${local.prefix}-${var.repository_name}"
   image_tag_mutability = var.immutable ? "IMMUTABLE" : "MUTABLE"
 
   image_scanning_configuration {
